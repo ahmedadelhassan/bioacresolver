c5aad742cff26ac713d1b7869ad17a96dde21a5d
Ahmed Hassan <ahmed.hassan@leaseplan.com>
2022-03-09 23:38:28 +0100

    adjusting license

--- create_bucket.py
+++ create_bucket.py
@@ -4,29 +4,29 @@ from minio import Minio
 from minio.error import InvalidResponseError
 
 accessID = os.environ.get('AWS_ACCESS_KEY_ID')
-accessSecret =  os.environ.get('AWS_SECRET_ACCESS_KEY')
-minioUrl =  os.environ.get('MLFLOW_S3_ENDPOINT_URL')
-bucketName =  os.environ.get('AWS_BUCKET_NAME')
+accessSecret = os.environ.get('AWS_SECRET_ACCESS_KEY')
+minioUrl = os.environ.get('MLFLOW_S3_ENDPOINT_URL')
+bucketName = os.environ.get('AWS_BUCKET_NAME')
 
-if accessID == None:
+if accessID is None:
     print('[!] AWS_ACCESS_KEY_ID environment variable is empty! run \'source .env\' to load it from the .env file')
     exit(1)
 
-if accessSecret == None:
+if accessSecret is None:
     print('[!] AWS_SECRET_ACCESS_KEY environment variable is empty! run \'source .env\' to load it from the .env file')
     exit(1)
 
-if minioUrl == None:
-    print('[!] MLFLOW_S3_ENDPOINT_URL environment variable is empty! run \'source .env\' to load it from the .env file')
+if minioUrl is None:
+    print(
+        '[!] MLFLOW_S3_ENDPOINT_URL environment variable is empty! run \'source .env\' to load it from the .env file')
     exit(1)
 
-    
-if bucketName == None:
+if bucketName is None:
     print('[!] AWS_BUCKET_NAME environment variable is empty! run \'source .env\' to load it from the .env file')
     exit(1)
 
 minioUrlHostWithPort = minioUrl.split('//')[1]
-print('[*] minio url: ',minioUrlHostWithPort)
+print('[*] minio url: ', minioUrlHostWithPort)
 
 s3Client = Minio(
     minioUrlHostWithPort,
